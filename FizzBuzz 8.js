bfa467c74c98c62452187d5ea7f3a8267e8eb3ee
emmadalgaard <54903673+emmadalgaard@users.noreply.github.com>
2019-09-13 14:26:43 +0200

    Add files via upload

---
+++ FizzBuzz 8.js
@@ -0,0 +1,4 @@
+for (var i=1; i < 100; i++){
+    if (i % 8 == 0) console.log("FizzBuzz");
+    else console.log(i);
+}
---
+++ Some code - what does it.js
@@ -0,0 +1,10 @@
+var persons = ["Caroline","Stine","Sanel","Frederik","Aske"];
+persons.forEach(function(person){
+    console.log(persons);
+});
+
+var i = persons.length-1;
+while(i >= 0){
+    console.log(persons[i]);
+    i--;
+}
